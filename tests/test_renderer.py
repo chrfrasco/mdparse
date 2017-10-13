3ec2c3b5229513f4a684eb86522d43aa94ee72b0
rollerbrick <mark.roller@rdbrck.com>
2017-10-13 12:11:39 -0700

    Added generic renderer/token and changed format to work with python3 < 3.6.

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -29,6 +29,48 @@ class TestHTMLRenderer(unittest.TestCase):
 
         self.assertEqual(html, '<h1>Hello, world!</h1>')
 
+    def test_generic_empty(self):
+        tokens = [GenericToken(children=[], token_type='p')]
+        html = HTMLRenderer.render_tokens(tokens)
+
+        self.assertEqual(html, '<p></p>')
+
+    def test_generic_paragrpah(self):
+        tokens = [GenericToken(children=[TextToken(value='Hello, world!')], token_type='p')]
+        html = HTMLRenderer.render_tokens(tokens)
+
+        self.assertEqual(html, '<p>Hello, world!</p>')
+
+    def test_generic_italic(self):
+        tokens = [GenericToken(children=[TextToken(value='Hello, world!')], token_type='i')]
+        html = HTMLRenderer.render_tokens(tokens)
+
+        self.assertEqual(html, '<i>Hello, world!</i>')
+
+    def test_generic_strikethrough(self):
+        tokens = [GenericToken(children=[TextToken(value='Hello, world!')], token_type='s')]
+        html = HTMLRenderer.render_tokens(tokens)
+
+        self.assertEqual(html, '<s>Hello, world!</s>')
+
+    def test_generic_bold(self):
+        tokens = [GenericToken(children=[TextToken(value='Hello, world!')], token_type='b')]
+        html = HTMLRenderer.render_tokens(tokens)
+
+        self.assertEqual(html, '<b>Hello, world!</b>')
+
+    def test_generic_underline(self):
+        tokens = [GenericToken(children=[TextToken(value='Hello, world!')], token_type='u')]
+        html = HTMLRenderer.render_tokens(tokens)
+
+        self.assertEqual(html, '<u>Hello, world!</u>')
+
+    def test_generic_chain(self):
+        tokens = [GenericToken(children=[GenericToken(children=[TextToken(value='Hello, world!')], token_type='b')], token_type='p')]
+        html = HTMLRenderer.render_tokens(tokens)
+
+        self.assertEqual(html, '<p><b>Hello, world!</b></p>')
+
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -9,6 +9,9 @@ class TestTokens(unittest.TestCase):
         self.assertRaises(ValueError, HeadingToken, children=[], level=0)
         self.assertRaises(ValueError, HeadingToken, children=[], level=7)
 
+    def test_invalid_generic_token_type(self):
+        self.assertRaises(ValueError, GenericToken, children=[], token_type='bad')
+
 
 if __name__ == '__main__':
     unittest.main()
